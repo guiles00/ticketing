f3b6f987eb0536534ab0202ac4409855fac9b2ee
Guillermo Caserotto <gcaserotto@tsjbaires.gov.ar>
2021-05-10 08:33:04 -0300

    another change

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -10,7 +10,7 @@ it("response with details about the current user", async () => {
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
     .send()
-    .expect(200);
+    .expect(201);
     
     expect(response.body.currentUser.email).toEqual("test@test.com");
 });
